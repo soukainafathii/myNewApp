0c2944d93870598245914c9b3953fc962a52bcd1
hamza Sibella <hamzasibella@gmail.com>
2021-06-06 16:29:14 +0100

    second commit

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -15,7 +15,7 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(" test ");
+        System.out.println("test ");
     }
     
 }
